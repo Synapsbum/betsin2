35028dc40e0226824338dd69db56a81e0029a0f6
Synapsbum <emreemre2@icloud.com>
2024-05-26 13:39:48 +0300

    deneme7
    deneme7

--- app/javascript/v3/views/login/Index.vue
+++ app/javascript/v3/views/login/Index.vue
@@ -4,19 +4,19 @@
   >
     <section class="max-w-5xl mx-auto">
       <img
-        :src="https://betsin42.com/img/logos/Logo.svg"
-        :alt="Betsin"
+        :src="globalConfig.logo"
+        :alt="globalConfig.installationName"
         class="block w-auto h-8 mx-auto dark:hidden"
       />
       <img
-        v-if="https://betsin42.com/img/logos/Logo.svg"
-        :src="https://betsin42.com/img/logos/Logo.svg"
-        :alt="Betsin"
+        v-if="globalConfig.logoDark"
+        :src="globalConfig.logoDark"
+        :alt="globalConfig.installationName"
         class="hidden w-auto h-8 mx-auto dark:block"
       />
       <h2
         class="mt-6 text-3xl font-medium text-center text-slate-900 dark:text-woot-50"
-      >BEtsin CRM'ye Hosgeldiniz!
+      >Betsin CRM'ye Hoşgeldiniz!
       </h2>
       <p
         v-if="showSignupLink"
